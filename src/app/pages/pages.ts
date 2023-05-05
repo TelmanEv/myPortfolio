afbe06d057bddfb64b87f34e637b68f1df66654a
Telman Evdaev <116366648+TelmanEv@users.noreply.github.com>
2023-05-05 15:33:55 +0300

    Update pages.ts

--- src/app/pages/pages.ts
+++ src/app/pages/pages.ts
@@ -1,71 +1,9 @@
-import React, { useEffect } from "react";
-
-export default function Overview() {
-  useEffect(() => {
-    document.title = "Overview - Telman Evdaev";
-  }, []);
-
-  return (
-    // Component JSX code here
-  );
-}
-
-export default function Skills() {
-  useEffect(() => {
-    document.title = "Skills - Telman Evdaev";
-  }, []);
-
-  return (
-    // Component JSX code here
-  );
-}
-
-export default function Experience() {
-  useEffect(() => {
-    document.title = "Experience - Telman Evdaev";
-  }, []);
-
-  return (
-    // Component JSX code here
-  );
-}
-
-export default function Education() {
-  useEffect(() => {
-    document.title = "Education - Telman Evdaev";
-  }, []);
-
-  return (
-    // Component JSX code here
-  );
-}
-
-export default function Projects() {
-  useEffect(() => {
-    document.title = "Projects - Telman Evdaev";
-  }, []);
-
-  return (
-    // Component JSX code here
-  );
-}
-
-export default function Certificates() {
-  useEffect(() => {
-    document.title = "Certificates - Telman Evdaev";
-  }, []);
-
-  return (
-    // Component JSX code here
-  );
-}
-
-export default function Accomplishments() {
-  useEffect(() => {
-    document.title = "Accomplishments - Telman Evdaev";
-  }, []);
-
-  return (
-    // Component JSX code here
-  );
-}
+export const pages = [
+  { index: 0, name: overview' },
+  { index: 1, name: 'skills.md', route: '/skills' },
+  { index: 2, name: 'experience.md', route: '/experience' },
+  { index: 3, name: 'education.md', route: '/education' },
+  { index: 4, name: 'projects.md', route: '/projects' },
+  { index: 5, name: 'certificates.md', route: '/certificates' },
+  { index: 6, name: 'accomplishments.md', route: '/accomplishments' },
+];
